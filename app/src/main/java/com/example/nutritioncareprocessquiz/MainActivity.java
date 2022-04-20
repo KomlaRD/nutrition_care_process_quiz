daa59dcf50cb6cfd618ddbacab276116fc69b322
komlaRD <ankueric1@gmail.com>
2022-04-20 01:04:42 +0000

    Added a quiz summary activity and a label for the activity

--- app/src/main/java/com/example/nutritioncareprocessquiz/MainActivity.java
+++ app/src/main/java/com/example/nutritioncareprocessquiz/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.nutritioncareprocessquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitButton(View view){
+        //Send explicit intent to quiz summary activity
+        Intent i = new Intent(this, QuizSummary.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/nutritioncareprocessquiz/QuizSummary.java
@@ -0,0 +1,14 @@
+package com.example.nutritioncareprocessquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuizSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_summary);
+    }
+}
