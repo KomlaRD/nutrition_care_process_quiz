ed4f3cbc667f8042bc25cb75051a7d1a05f1341e
komlaRD <ankueric1@gmail.com>
2022-04-25 21:55:07 +0000

    Added click method for question one

--- app/src/main/java/com/example/nutritioncareprocessquiz/MainActivity.java
+++ app/src/main/java/com/example/nutritioncareprocessquiz/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +21,27 @@ public class MainActivity extends AppCompatActivity {
         //Send explicit intent to quiz summary activity
         Intent i = new Intent(this, QuizSummary.class);
         startActivity(i);
+    }
+
+    public void quizOneButton(View view){
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which answer has been checked
+        switch(view.getId()){
+            case R.id.answer_one:
+                if (checked) {
+                    score = 1;
+                }
+                break;
+            case R.id.answer_two:
+            case R.id.answer_three:
+            case R.id.answer_four:
+                if (checked){
+                    score = 0;
+                }
+                break;
+        }
+
     }
 }
